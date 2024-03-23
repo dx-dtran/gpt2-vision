01a164e5a2ada7e89b5faff10303ee3cb338e33d
daniel <dxdtran@outlook.com>
2024-03-23 13:38:06 -0700

    Initial commit

---
+++ gpt.py
@@ -0,0 +1,165 @@
+import math
+import torch
+import torch.nn as nn
+from dataclasses import dataclass
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024
+    vocab_size: int = 50304
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: bool = True
+
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+
+    def forward(self, x, mask=None, cache=None):
+        b, t, c = x.size()
+
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(b, t, self.n_head, c // self.n_head).transpose(1, 2)
+        q = q.view(b, t, self.n_head, c // self.n_head).transpose(1, 2)
+        v = v.view(b, t, self.n_head, c // self.n_head).transpose(1, 2)
+
+        if cache is not None:
+            key_cache, value_cache = cache
+            k = torch.cat([key_cache, k], dim=2)
+            v = torch.cat([value_cache, v], dim=2)
+
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+
+        if mask is not None:
+            att = att + mask
+
+        att = torch.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+        out = att @ v
+        out = out.transpose(1, 2).contiguous().view(b, t, c)
+
+        out = self.resid_dropout(self.c_proj(out))
+        return out, (k, v)
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.gelu = nn.GELU()
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd, eps=1e-5, elementwise_affine=config.bias)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd, eps=1e-5, elementwise_affine=config.bias)
+        self.mlp = MLP(config)
+
+    def forward(self, x, mask=None, cache=None):
+        norm = self.ln_1(x)
+        att, cache = self.attn(norm, mask=mask, cache=cache)
+        x = x + att
+        norm = self.ln_2(x)
+        mlp = self.mlp(norm)
+        x = x + mlp
+        return x, cache
+
+
+class GPT(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        self.config = config
+
+        self.wte = nn.Embedding(config.vocab_size, config.n_embd)
+        self.wpe = nn.Embedding(config.block_size, config.n_embd)
+        self.drop = nn.Dropout(config.dropout)
+        self.h = nn.ModuleList([Block(config) for _ in range(config.n_layer)])
+        self.ln_f = nn.LayerNorm(config.n_embd, eps=1e-5, elementwise_affine=config.bias)
+
+    def _forward_transformer_blocks(
+        self, x, pos, mask=None, cache=None, build_cache=False
+    ):
+        tok_emb = self.wte(x)
+        pos_emb = self.wpe(pos)
+        x = self.drop(tok_emb + pos_emb)
+        kv_cache = []
+
+        if cache is not None:
+            for i in range(len(cache)):
+                x, cache[i] = self.h[i](x, mask=None, cache=cache[i])
+        else:
+            for block in self.h:
+                x, curr_cache = block(x, mask=mask)
+                if build_cache:
+                    kv_cache.append(curr_cache)
+
+        x = self.ln_f(x)
+        return x, kv_cache if build_cache else cache
+
+    def _create_causal_mask(self, length: int):
+        mask = torch.tril(torch.ones((length, length), dtype=torch.float32))
+        return mask.view(1, 1, length, length).to(self.wte.weight.dtype).to(self.wte.weight.device)
+
+    def _sample_next_token(self, x, temperature):
+        logits = x[:, -1:] @ self.wte.weight.T
+        y = logits[:, -1, :]
+        y = torch.multinomial(torch.softmax(y * (1 / temperature), dim=-1), num_samples=1)
+        return y
+
+    def generate(self, x, max_new_tokens=256, temperature=0.8):
+        _, t = x.size()
+        pos = torch.arange(0, t, dtype=torch.long, device=x.device)
+        mask = self._create_causal_mask(t)
+        x, cache = self._forward_transformer_blocks(x, pos, mask=mask, build_cache=True)
+        y = self._sample_next_token(x, temperature)
+        position = t
+        yield y
+
+        for _ in range(max_new_tokens):
+            position += 1
+            x = y.unsqueeze(1)
+            x, cache = self._forward_transformer_blocks(x, torch.tensor([position], dtype=torch.long, device=x.device), cache=cache)
+            y = self._sample_next_token(x, temperature)
+            yield y
+
+    def forward(self, x, targets=None):
+        b, t = x.size()
+        assert (
+            t <= self.config.block_size
+        ), f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=x.device)
+
+        mask = self._create_causal_mask(t)
+        x, _ = self._forward_transformer_blocks(x, pos, mask=mask)
+
+        return x @ self.wte.weight.T
+
+    def loss(self, x, y):
+        logits = self(x)
+        loss = nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1))
+        return loss.mean()
