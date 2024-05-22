4589bd680d540ced21572becbf51d2aaf010a5fc
daniel <dxdtran@outlook.com>
2024-05-21 20:45:02 -0700

    clean up clip implementation

--- clip_test.py
+++ clip_test.py
@@ -1,6 +1,7 @@
 import torch
 import clip
 import clip_helper
+from clip_helper_clean import load as load_clean
 from PIL import Image
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -10,10 +11,17 @@ model2 = clip.load_clip()
 image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
 # text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
 
+model_clean, preprocess_clean = load_clean("ViT-B/32", device=device)
+image_clean = preprocess_clean(Image.open("CLIP.png")).unsqueeze(0).to(device)
+
 with torch.no_grad():
     image_features = model.encode_image(image)
 
     image_features_my = model2.encode_image(image)
+
+    image_features_clean = model_clean.encode_image(image)
+
+    torch.save(model_clean.state_dict(), "clip_simplified.pt")
 
     print("hi")
     # text_features = model.encode_text(text)
