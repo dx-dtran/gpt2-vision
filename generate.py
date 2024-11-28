9067ad874fef613dee9f61a97489b24ca939cedd
daniel <dxdtran@outlook.com>
2024-11-28 07:54:29 -0800

    use old vision language connector

--- generate.py
+++ generate.py
@@ -94,10 +94,10 @@ def save_image_and_caption_to_png(folder, image, caption, image_filename):
 
 if __name__ == "__main__":
     image_folder = "input_images"
-    output_folder = "outputs-11-27-2024-0424pm"
+    output_folder = "outputs-11-28-2024-0739pm"
 
     gpt_weights_path = "gpt2.pt"
-    connector_weights_path = "connector_weights_11000.pt"
+    connector_weights_path = "vl_connector.pt"
 
     vision_encoder, preprocess, model, connector, tokenizer = load_models_and_tokenizer(
         gpt_weights_path, connector_weights_path
